7125ab3f80dd68ddfcf973ff3a3ac526deaf6e2c
Alexander Larsson <alexl@redhat.com>
2011-09-21 15:16:16 +0200

    Add makershared.c

---
+++ makershared.c
@@ -0,0 +1,24 @@
+#include <sys/mount.h>
+#include <sched.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int
+main (int argc,
+      char **argv)
+{
+  int mount_count;
+  int res;
+
+  res = mount ("/", "/",
+	       NULL, MS_SHARED|MS_REC, NULL);
+
+  if (res != 0) {
+    perror ("Failed to make rshared");
+    return 1;
+  }
+  return 0;
+}
